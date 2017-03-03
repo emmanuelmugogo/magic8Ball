5246ac471b49a193425ba5c4d36667caf3e93989
emmanuelmugogo <mugoclan87@gmail.com>
2017-03-03 09:37:00 -0500

    initial commit

---
+++ magic8Ball.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function() {
+            $("#triangle").hide();
+            $("#answerContainer").hide();
+
+            $("#button").click(function(){
+               $(".sphere").effect( "shake", {times:4}, 1000 );
+               $("#number8").hide();
+               $("#triangle").show(1000);
+               $("#answerContainer").show(1300);
+
+               var answer = answers[Math.floor(Math.random() * answers.length)];
+              document.getElementById('answerContainer').innerHTML = answer;
+            });
+        
+         });
+
+
+var answers = [
+  'Maybe.', 'Certainly not.', 'I hope so.', 'Not in your wildest dreams.',
+  'There is a good chance.', 'Quite likely.', 'I think so.', 'I hope not.',
+  'I hope so.', 'Never!', 'Fuhgeddaboudit.', 'Ahaha! Really?!?', 'Pfft.',
+  'Sorry, bucko.', 'Hell, yes.', 'Hell to the no.', 'The future is bleak.',
+  'The future is uncertain.', 'I would rather not say.', 'Who cares?',
+  'Possibly.', 'Never, ever, ever.', 'There is a small chance.', 'Yes!'];
+
+// document.getElementById('answerButton').onclick = function () {
+// var answer = answers[Math.floor(Math.random() * answers.length)];
+//     document.getElementById('answerContainer').innerHTML = answer;
+// };
