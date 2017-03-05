0d7d2e3e293523619fdbe176aa5d3e02407ec6e4
emmanuelmugogo <mugoclan87@gmail.com>
2017-03-04 22:14:04 -0500

    may be final lol

--- magic8Ball.js
+++ magic8Ball.js
@@ -8,11 +8,15 @@ $(document).ready(function() {
                $(".sphere").effect( "shake", {times:4}, 1000 );
                $("#number8").hide().delay(4500).fadeIn();
                $("#triangle").show(1000).delay(3000).fadeOut();
-               $("#answerContainer").show(1300).delay(1500).fadeOut();
+               $("#answerContainer").show(1000).delay(1500).fadeOut();
+               $("input:text").fadeOut(1000);
+               $("input:text").fadeIn(2000);
 
                var answer = answers[Math.floor(Math.random() * answers.length)];
               document.getElementById('answerContainer').innerHTML = answer;
             });
+
+          
         
          });
 
